e62810943e6765c2c34cd5df717f0c549e74cf4d
Deutro <patrick.engelkes@gmail.com>
2015-03-06 11:30:13 +0100

    Change SessionService to reflect static variable changes in structs.

--- AlleNeune/SessionService.swift
+++ AlleNeune/SessionService.swift
@@ -12,11 +12,15 @@ class SessionService {
     
     func logIn(email : String, password : String) -> Bool {
         var semaphore = dispatch_semaphore_create(0)
-        let session = Session()
         let apiPostHandler = ApiPostHandler()
         
-        var params = [session.ROOT: [session.EMAIL : email, session.PASSWORD : password]]
-        var url = apiPostHandler.HOST + session.GENERIC_URL
+        var params = [
+            Session.ROOT: [
+                Session.EMAIL : email,
+                Session.PASSWORD : password
+            ]
+        ]
+        var url = apiPostHandler.HOST + Session.GENERIC_URL
         var success = false;
         apiPostHandler.apiPost(params, url: url) { (succeeded, postResponse) -> () in
             if succeeded {
