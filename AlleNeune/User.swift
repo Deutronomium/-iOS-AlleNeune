46ca15eda009e613fce1e7c21f5f00c21a01844b
Deutro <patrick.engelkes@gmail.com>
2015-03-06 07:22:36 +0100

    Add user struct for API calls.

--- AlleNeune/User.swift
+++ AlleNeune/User.swift
@@ -6,4 +6,21 @@
 import Foundation
 
 struct User {
+    //api parameters
+    let ROOT = "user"
+    let ID = "id"
+    let USER_NAME = "user_name"
+    let FIRST_NAME = "first_name"
+    let LAST_NAME = "last_name"
+    let PHONE_NUMBER = "phone_number"
+    let EMAIL = "email"
+    let STREET = "street"
+    let CITY = "city"
+    let PASSWORD = "password"
+    let PASSWORD_CONFIRMATION = "password_confirmation"
+
+    //URLs
+    let GENERIC_URL = "/users"
+    let VALIDITY = GENERIC_URL + "/validity"
+    let GET_USER_BY_CLUB_NAME = GENERIC_URL + "/user_club";
 }
